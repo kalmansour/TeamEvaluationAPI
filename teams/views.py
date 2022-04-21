f7a99e5189c39c0ad5500d8c2acafdac7e765753
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-04-22 00:07:20 +0300

    project model, add project serializer, add project view and add project url

--- teams/views.py
+++ teams/views.py
@@ -1,9 +1,9 @@
-from .serializers import CreateSemesterSerializer, UserAdminRegisterSerializer,MyTokenObtainPairSerializer,SemesterListSerializer
+from .serializers import CreateSemesterSerializer, UserAdminRegisterSerializer,MyTokenObtainPairSerializer,SemesterListSerializer, ProjectListSerializer, CreateProjectSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import IsAdminUser
 
-from .models import Semester
+from .models import Semester, Project
 
 class UserAdminRegister(CreateAPIView):
     serializer_class = UserAdminRegisterSerializer
@@ -21,3 +21,15 @@ class CreateSemesterView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(added_by=self.request.user)
+
+class ProjectListView(ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectListSerializer
+
+class CreateProjectView(CreateAPIView):
+    serializer_class = CreateProjectSerializer
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer ):
+        semester = Semester.objects.get(id=self.kwargs['semester_id'])
+        serializer.save(semester=semester)
